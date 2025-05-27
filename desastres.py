07711192c4cc5d83bfafa643688bb0048f739847
”Patrick <rm565315@fiap.com.br>
2025-05-27 10:46:18 -0300

    Primeiro commit GS

---
+++ desastres.py
@@ -0,0 +1,17 @@
+# Solicita a quantidade de desastres
+quantidade_desastres = int(input("Digite a quantidade de desastres registrados: "))
+
+# Inicializa listas para armazenar informações sobre os desastres
+tipos_desastres = []
+paises = []
+cidades = []
+bairros = []
+ruas = []
+qt_pessoas_afetadas = []
+
+# Quantidade de pessoas afetadas por categoria
+criancas = []
+adultos = []
+idosos = []
+mobilidade_reduzida = []
+feridos = []
